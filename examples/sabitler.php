c3ebd6ab54b69cd2e7621b2fae3bfee4aa414375
“sadikturan” <sadikturan@gmail.com>
2023-02-13 12:23:48 +0300

    2_7_sabitler.mp4

---
+++ examples/sabitler.php
@@ -0,0 +1,12 @@
+<?php
+    // PHP Sabitler: 
+    // Sabitler veri saklamak için kullanılır ancak tanımlanan değer değiştirilemez.
+
+    // define("baglanti", "mysql bağlantı cümlesi");
+    const baglanti = "mysql bağlantı cümlesi";
+    const username = "sadikturan";
+    const password = "1234";
+
+    echo baglanti;
+
+?>
