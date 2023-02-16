33a6e4a4f295ac0df57524c52c077607f13b7d76
“sadikturan” <sadikturan@gmail.com>
2023-02-16 11:23:07 +0300

    saved

---
+++ examples/bolum-3/aritmetik-opetorler.php
@@ -0,0 +1,48 @@
+<?php
+    /*  
+        +       Toplama                 $x + $y         
+        -       Çıkarma                 $x - $y     
+        *       Çarpma                  $x * $y         
+        /       Bölme                   $x / $y         
+        %       Mod Alma                $x % $y         
+        **      Üs Alma                 $x ** $y
+        ++$x    İşlem öncesi arttırma   
+        --$x    İşlem öncesi azaltma    
+        $x++    İşlem sonrası arttırma  
+        $x--    İşlem sonrası azaltma   
+    */
+
+    $a = 2;
+    $b = 5;
+
+    echo $a + $b;
+    echo "<br>";
+
+    echo $a - $b;
+    echo "<br>";
+
+    echo $a * $b;
+    echo "<br>";
+
+    echo $a / $b;
+    echo "<br>";
+
+    echo $a % $b;
+    echo "<br>";
+
+    echo $a ** $b;
+    echo "<br>";
+
+    echo $a++;
+    echo "<br>";
+
+    echo ++$a;
+    echo "<br>";
+
+    echo $a--;
+    echo "<br>";
+
+    echo $a;
+    echo "<br>";
+?>
+
