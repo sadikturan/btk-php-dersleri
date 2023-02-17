1488b6911f2ad7aa77627d30515ae3358cfe8d89
“sadikturan” <sadikturan@gmail.com>
2023-02-17 13:05:48 +0300

    saved

---
+++ examples/bolum-4/ifelse.php
@@ -0,0 +1,30 @@
+<?php
+
+    // Koşullu Durumlar
+
+        // 1- if-else
+        // 2- Switch Bloğu
+        // 3- Ternary Operatörü
+
+    $username = "admin";
+    $password = "1234";
+
+    if ($username == "admin") {
+
+        if ($password == "1234") {
+            echo "Merhaba BTK";
+        } else {
+            echo "parola yanlış";
+        }   
+
+    } else {
+        echo "username yanlış";
+    }
+
+    #soru1
+    #soru2
+    #soru3
+    #else
+
+
+?>
