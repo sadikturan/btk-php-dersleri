2fb942dcbd0dc0b3e0854d9c14875ac4b2e37eec
“sadikturan” <sadikturan@gmail.com>
2023-02-20 11:25:21 +0300

    saved

---
+++ examples/bolum-6/fonksiyon-nedir.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Fonksiyon Nedir?
+
+    // Built-in Fonksiyonlar
+
+    echo date('Y/m/d');
+    echo "<br>";
+    echo strlen("Merhaba BTK");
+
+    // 3rd libs (fonksiyon)
+    // Kullanıcı Tanımlı Fonksiyonlar
+
+?>
