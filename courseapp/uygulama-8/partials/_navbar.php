a2a8e94712c0c1c50e3478792322273fb2804e87
“sadikturan” <sadikturan@gmail.com>
2023-02-26 11:53:46 +0300

    saved

---
+++ courseapp/uygulama-8/partials/_navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">CourseApp</a>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a href="/" class="nav-link active">Anasayfa</a>
+            </li>
+            <li class="nav-item">
+                <a href="/" class="nav-link">Kurslar</a>
+            </li>
+        </ul>
+    </div>
+</nav>
