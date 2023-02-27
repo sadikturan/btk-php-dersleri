56c65b3d89b582b4bb7b4494a18df0831aa4bcd6
“sadikturan” <sadikturan@gmail.com>
2023-02-27 12:35:58 +0300

    saved

---
+++ courseapp/uygulama-9/libs/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+function kursEkle(&$kurslar, string $baslik,string $altBaslik,string $resim,string $yayinTarihi,int $yorumSayisi=0,int $begeniSayisi=0, bool $onay=true) {
+    $yeni_kurs[count($kurslar) + 1] = array(
+        "baslik" => $baslik,
+        "altBaslik" => $altBaslik,
+        "resim" => $resim,
+        "yayinTarihi" => $yayinTarihi,
+        "yorumSayisi" => $yorumSayisi,
+        "begeniSayisi" => $begeniSayisi,
+        "onay" => $onay
+    );
+
+    $kurslar = array_merge($kurslar, $yeni_kurs);
+}
+
+function urlDuzenle($baslik) {
+    return str_replace([" ","ç","@","."],["-","c","","-"],strtolower($baslik));
+}
+
+function kisaAciklama($altBaslik) {
+    if (strlen($altBaslik) > 50) {
+        return substr($altBaslik,0,50)."..."; 
+    } else {
+        return $altBaslik;
+    }
+}
+?>
