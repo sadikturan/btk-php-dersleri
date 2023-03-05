b8d51b81c6e1e73bb9cef365650b37a8b8a8c436
“sadikturan” <sadikturan@gmail.com>
2023-03-05 08:30:36 +0300

    saved

--- examples/bolum-9/multiple-upload.php
+++ examples/bolum-9/multiple-upload.php
@@ -1,21 +1,56 @@
 <?php
 
     if(isset($_POST["btnFileUpload"]) && $_POST["btnFileUpload"]=="Upload") {
+
         $dosya_adeti = count($_FILES["fileToUpload"]["name"]);
-        for($i=0; $i < $dosya_adeti; $i++) {
-            $fileTmpPath = $_FILES["fileToUpload"]["tmp_name"][$i];
-            $fileName = $_FILES["fileToUpload"]["name"][$i];
+        $maxFileSize = (1024 * 1024) * 1;
+        $fileTypes = array("image/png","image/jpg","image/jpeg");
+        $uploadOk = true;
 
-            $dest_path = "images/".$fileName;
+        if($dosya_adeti > 2) {
+            $uploadOk = false;
+            echo "max. 2 dosya yükleyebilirsiniz.";
+        }
 
-            if(move_uploaded_file($fileTmpPath, $dest_path)) {
-                echo $fileName." dosyası yüklendi"."<br>";
-            } else {
-                echo $fileName."dosya yükleme hatası"."<br>";
+        if($uploadOk) {
+            for($i=0; $i < $dosya_adeti; $i++) {
+                
+                $fileTmpPath = $_FILES["fileToUpload"]["tmp_name"][$i];
+                $fileName = $_FILES["fileToUpload"]["name"][$i];
+                $fileSize = $_FILES["fileToUpload"]["size"][$i];
+                $fileType = $_FILES["fileToUpload"]["type"][$i];
+                
+
+                if(in_array($fileType, $fileTypes)) {
+
+                    if($fileSize > $maxFileSize) {
+                        echo "max dosya botuyu 1 mb olmalıdır."."<br>";
+                    } else {
+
+                        $dosyaAdi_Arr = explode(".", $fileName);
+                        $dosyaAdi_uzantisiz = $dosyaAdi_Arr[0];
+                        $dosyaAdi_uzantisi = $dosyaAdi_Arr[1];
+
+                        $yeniDosyaAdi = $fileName."-".rand(0, 9999999).".".$dosyaAdi_uzantisi;
+                        $dest_path = "images/".$yeniDosyaAdi;
+
+                        if(move_uploaded_file($fileTmpPath, $dest_path)) {
+                            echo $yeniDosyaAdi." dosyası yüklendi"."<br>";
+                        } else {
+                            echo $yeniDosyaAdi."dosya yükleme hatası"."<br>";
+                        }
+                    }
+
+                } else {
+                    echo "dosya uzantısı kabul edilmiyor."."<br>";
+                    echo "kabul edilen dosya tipleri: ".implode(".", $fileTypes)."<br>";
+                }
+                
             }
         }
     }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
