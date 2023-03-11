62d0d68e9885014a2b229a2c98cf1b281c4b5077
“sadikturan” <sadikturan@gmail.com>
2023-03-11 08:24:28 +0300

    saved

--- courseapp/uygulama-14/libs/functions.php
+++ courseapp/uygulama-14/libs/functions.php
@@ -8,8 +8,10 @@ function getDb() {
     return $data;
 }
 
-function kursEkle(&$kurslar, string $baslik,string $altBaslik,string $resim,string $yayinTarihi,int $yorumSayisi=0,int $begeniSayisi=0, bool $onay=true) {
-    $yeni_kurs[count($kurslar) + 1] = array(
+function kursEkle(string $baslik,string $altBaslik,string $resim,string $yayinTarihi,int $yorumSayisi=0,int $begeniSayisi=0, bool $onay=true) {
+    $db = getDb();
+    
+    array_push($db["kurslar"], array(
         "baslik" => $baslik,
         "altBaslik" => $altBaslik,
         "resim" => $resim,
@@ -17,9 +19,11 @@ function kursEkle(&$kurslar, string $baslik,string $altBaslik,string $resim,stri
         "yorumSayisi" => $yorumSayisi,
         "begeniSayisi" => $begeniSayisi,
         "onay" => $onay
-    );
+    ));
 
-    $kurslar = array_merge($kurslar, $yeni_kurs);
+    $myfile  = fopen("db.json","w");
+    fwrite($myfile, json_encode($db, JSON_PRETTY_PRINT));
+    fclose($myfile);
 }
 
 function urlDuzenle($baslik) {
