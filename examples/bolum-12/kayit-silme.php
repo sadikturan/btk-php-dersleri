9e1375e91c11e159e5b784de7de00f02fd2cdaa5
“sadikturan” <sadikturan@gmail.com>
2023-03-12 21:42:08 +0300

    saved

---
+++ examples/bolum-12/kayit-silme.php
@@ -0,0 +1,19 @@
+<?php
+
+    include "ayar.php";
+
+    // $query = "DELETE FROM kurslar WHERE id=6";
+    $query = "DELETE FROM kurslar WHERE id>6 and id<10";
+
+    $sonuc = mysqli_query($baglanti, $query);
+    $adet = mysqli_affected_rows($baglanti);
+
+    if($sonuc)  {
+        echo "veri silindi: ".$adet;
+    } else {
+        echo "veri silme hatası";
+    }
+
+
+    mysqli_close($baglanti);
+?>
