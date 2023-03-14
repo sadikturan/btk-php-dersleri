758b31b45548f98a6ad665a8d06f881f429f23e3
“sadikturan” <sadikturan@gmail.com>
2023-03-14 15:18:42 +0300

    saved

--- courseapp/uygulama-15/libs/functions.php
+++ courseapp/uygulama-15/libs/functions.php
@@ -13,7 +13,7 @@ function getCategories() {
 function getCourses() {
     include "ayar.php";
 
-    $query = "SELECT k.id,k.baslik,k.resim,k.onay,c.kategori_adi from kurslar k inner join kategoriler c on k.id=c.id";
+    $query = "SELECT k.id,k.baslik,k.resim,k.onay,c.kategori_adi from kurslar k inner join kategoriler c on k.kategori_id=c.id";
     $sonuc = mysqli_query($baglanti,$query);
     mysqli_close($baglanti);
     return $sonuc;
@@ -46,10 +46,10 @@ function editCategory(int $id, string $category) {
     return $sonuc;
 }
 
-function editCourse(int $id, string $baslik, string $altBaslik, string $resim, int $onay) {
+function editCourse(int $id, string $baslik, string $altBaslik, string $resim,int $kategori_id, int $onay) {
     include "ayar.php";
 
-    $query = "UPDATE kurslar SET baslik='$baslik', altBaslik='$altBaslik', resim='$resim',onay='$onay' WHERE id=$id";
+    $query = "UPDATE kurslar SET baslik='$baslik', altBaslik='$altBaslik',resim='$resim',kategori_id=$kategori_id,onay=$onay WHERE id=$id";
     $sonuc = mysqli_query($baglanti,$query);
     mysqli_close($baglanti);
     return $sonuc;
@@ -78,13 +78,13 @@ function createCategory(string $kategori) {
     return $stmt;
 }
 
-function createCourse(string $baslik, string $altBaslik, string $resim, int $yorumSayisi = 0, int $begeniSayisi=0,int $onay=0) {
+function createCourse(string $baslik, string $altBaslik, string $resim,int $kategori_id,int $yorumSayisi = 0, int $begeniSayisi=0,int $onay=0) {
     include "ayar.php";
 
-    $query = "INSERT INTO kurslar(baslik,altBaslik,resim,yorumSayisi,begeniSayisi,onay) VALUES (?,?,?,?,?,?)";
+    $query = "INSERT INTO kurslar(baslik,altBaslik,resim,kategori_id,yorumSayisi,begeniSayisi,onay) VALUES (?,?,?,?,?,?,?)";
     $stmt = mysqli_prepare($baglanti,$query);
 
-    mysqli_stmt_bind_param($stmt, 'sssiii', $baslik,$altBaslik,$resim,$yorumSayisi,$begeniSayisi,$onay);
+    mysqli_stmt_bind_param($stmt, 'sssiiii', $baslik,$altBaslik,$resim,$kategori_id,$yorumSayisi,$begeniSayisi,$onay);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
 
