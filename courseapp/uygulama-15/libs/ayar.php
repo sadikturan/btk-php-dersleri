4193e47f74eb795c4189a1c8f9fa741126e75325
“sadikturan” <sadikturan@gmail.com>
2023-03-14 20:51:42 +0300

    saved

--- courseapp/uygulama-15/libs/ayar.php
+++ courseapp/uygulama-15/libs/ayar.php
@@ -1,10 +1,5 @@
 <?php
-    const host = "localhost";
-    const username = "root";
-    const password = "";
-    const database = "coursedb";
-
-    $baglanti = mysqli_connect(host, username, password, database);
+    $baglanti = mysqli_connect("localhost", "root", "", "coursedb");
 
     if(mysqli_connect_errno() > 0) {
         die("hata: ".mysqli_connect_errno());
