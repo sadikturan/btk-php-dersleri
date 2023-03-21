4e29ac58d13fe8d5d89cf345cb43141dbec74514
“sadikturan” <sadikturan@gmail.com>
2023-03-21 20:46:39 +0300

    saved

--- examples/bolum-17/classes/product.class.php
+++ examples/bolum-17/classes/product.class.php
@@ -15,6 +15,16 @@
             $stmt->execute(['id' => $productId]);
             return $stmt->fetch();
         }
+
+        public function createProduct($title, $description, $price) {
+            $sql = "INSERT INTO products(title,description,price) VALUES (:title,:description,:price)";
+            $stmt = $this->connect()->prepare($sql);
+            return $stmt->execute([
+                'title' => $title,
+                'description' => $description,
+                'price' => $price,
+            ]);
+        }
     }
 
 
---
+++ examples/bolum-17/includes/product-list.php
@@ -0,0 +1,28 @@
+<table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th style="width: 50px">id</th>
+                    <th>title</th>
+                    <th>price</th>
+                    <th>description</th>
+                    <th style="width: 150px"></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach($product->getProducts() as $item): ?>
+                    <tr>
+                        <td ><?php echo $item->id?></td>
+                        <td><?php echo $item->title?></td>
+                        <td><?php echo $item->price?></td>
+                        <td><?php echo $item->description?></td>
+                        <td >
+                            <a href="" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                    </tr>    
+                <?php endforeach;?>
+            </tbody>
+
+                
+        </table>
