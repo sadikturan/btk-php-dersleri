cd147c819a025477e86d6e1e4fbcd41d17492d36
“sadikturan” <sadikturan@gmail.com>
2023-03-21 20:45:41 +0300

    saved

--- examples/bolum-17/index.php
+++ examples/bolum-17/index.php
@@ -9,18 +9,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
     <title>Document</title>
 </head>
 <body>
-    <?php
 
-        $product = new Product();
+<div class="container my-3">
 
-        echo "<pre>";
-        // print_r($product->getProducts());
-        echo "<br>";
-        print_r($product->getProductById(2));
-    
-    ?>
+    <?php $product = new Product();?>
+
+    <?php if($product->getProducts()): ?>
+
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th style="width: 50px">id</th>
+                    <th>title</th>
+                    <th>price</th>
+                    <th>description</th>
+                    <th style="width: 150px"></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach($product->getProducts() as $item): ?>
+                    <tr>
+                        <td ><?php echo $item->id?></td>
+                        <td><?php echo $item->title?></td>
+                        <td><?php echo $item->price?></td>
+                        <td><?php echo $item->description?></td>
+                        <td >
+                            <a href="" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                    </tr>    
+                <?php endforeach;?>
+            </tbody>
+
+                
+        </table>
+
+    <?php else:?>
+        <div class="alert alert-warning">
+            Ürün bulunamadı.
+        </div>
+    <?php endif;?>
+
+    </div>
+
 </body>
 </html>
