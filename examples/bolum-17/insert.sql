b7f6dc6c8e931242035ed2706c8bccbaba9fa96b
“sadikturan” <sadikturan@gmail.com>
2023-03-21 12:57:07 +0300

    saved

---
+++ examples/bolum-17/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `products`(`title`, `price`, `description`) VALUES ('iphone 14', 20000, 'güzel telefon');
+INSERT INTO `products`(`title`, `price`, `description`) VALUES ('iphone 15', 30000, 'güzel telefon');
+INSERT INTO `products`(`title`, `price`, `description`) VALUES ('iphone 16', 40000, 'güzel telefon');
+INSERT INTO `products`(`title`, `price`, `description`) VALUES ('iphone 17', 50000, 'güzel telefon');
