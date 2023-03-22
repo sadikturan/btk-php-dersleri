a16a8402031dad311dafef56aa7aad692f142b0e
“sadikturan” <sadikturan@gmail.com>
2023-03-22 22:27:15 +0300

    saved

--- examples/bolum-17/includes/product-list.php
+++ examples/bolum-17/includes/product-list.php
@@ -17,7 +17,7 @@
                         <td><?php echo $item->price?></td>
                         <td><?php echo $item->description?></td>
                         <td >
-                            <a href="" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="edit-product.php?id=<?php echo $item->id?>" class="btn btn-primary btn-sm">Edit</a>
                             <a href="" class="btn btn-danger btn-sm">Delete</a>
                         </td>
                     </tr>    
